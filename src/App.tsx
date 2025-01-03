2aa63cd8d302d142c42ac83ffc06189de403df88
Rohanfizz <rohans0501@gmail.com>
2025-01-03 13:16:19 +0530

    initialise

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="border h-screen w-screen bg-primary text-white">
+            asd
+        </div>
+    );
+}
+
+export default App;
