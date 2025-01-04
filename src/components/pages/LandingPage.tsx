99c9d8fc4d44759607187098983fe62eebb8267b
adkood <ashutoshnegi195@gmail.com>
2025-01-05 00:04:20 +0530

    Task: landingpage modified

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import WhatOurStudentSays from "../ui/WhatOurStudentSays.tsx";
 import EnquirySection from "../ui/EnquirySection.tsx";
+import VisitCourses from "../ui/visitCourses/VisitCoursesHome.tsx"
+import WhyChooseCoc from "../ui/WhyChooseCoc.tsx"
+import FreeVideo from "../ui/FreeVideo.tsx"
 
 const LandingPage = () => {
     return (
-        <div className="">
+        <div>
             <EnquirySection/>
+            <VisitCourses />
+            <WhyChooseCoc />
+            <FreeVideo />
             <WhatOurStudentSays />
         </div>
     );
