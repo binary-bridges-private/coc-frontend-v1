7d6c12132d26cab386328c3043fbb352106b1062
adkood <ashutoshnegi195@gmail.com>
2025-01-06 01:32:25 +0530

    final landing page structure done

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -6,16 +6,19 @@ import FreeVideo from "../ui/FreeVideo.tsx";
 import WhyChooseCOC from "../ui/WhyChooseCoc.tsx";
 import VisitCoursesHome from "../ui/visitCourses/VisitCoursesHome.tsx";
 import Faculties from "../ui/faculty/FacultyHome.tsx";
+import TopBanner from "../ui/TopBanner.tsx";
+import NoticeBoardHome from "../ui/noticeBoard/NoticeBoardHome.tsx";
 
 const LandingPage = () => {
     return (
         <div className="">
             <EnquirySection />
-            
-            <ProductSectionLandingPage />
-            {/* <Faculties /> */}
+            <TopBanner />
+            <NoticeBoardHome />
             <VisitCoursesHome />
             <WhyChooseCOC />
+            <ProductSectionLandingPage />
+            <Faculties />
             <FreeVideo />
             <WhatOurStudentSays />
         </div>
