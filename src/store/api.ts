6dd3125351f81404c73ba09bf69193c8d0142698
adkood <ashutoshnegi195@gmail.com>
2025-04-12 03:49:27 +0530

    base url changed

--- src/store/api.ts
+++ src/store/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { store } from './index.ts';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/practice/api/v1',
+  baseURL: 'http://practice.coceducation.com/api/practice/v1',
   withCredentials: true,
 });
 
 export const apiRestricted = axios.create({
-  baseURL: 'http://localhost:8080/practice/api/v1',
+  baseURL: 'http://practice.coceducation.com/api/practice/v1',
   withCredentials: true
 });
 
