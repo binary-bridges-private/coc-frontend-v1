66b15c5b12c53ce4502861495e4d31992bb1103f
adkood <ashutoshnegi195@gmail.com>
2025-05-23 05:24:00 +0530

    api chnaged

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const devUrl = 'http://localhost:8080/practice/v1';
-const prodUrl = 'http://practice.coceducation.com/api/practice/v1';
+const prodUrl = 'http://65.2.81.17/api/practice/v1';
 
 export const api = axios.create({
   baseURL: prodUrl,
